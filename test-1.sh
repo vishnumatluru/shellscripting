62aec2d64e330c93ef8364306090fd7b691cfeb4
vishnurebel <vishnumatluru@gmail.com>
2023-06-23 14:18:31 +0530

    this is my commit

---
+++ test-1.sh
@@ -0,0 +1,21 @@
+from tinygrad.tensor import Tensor
+import tinygrad.nn.optim as optim
+
+class TinyBobNet:
+  def __init__(self):
+    self.l1 = Tensor.uniform(784, 128)
+    self.l2 = Tensor.uniform(128, 10)
+
+  def forward(self, x):
+    return x.dot(self.l1).relu().dot(self.l2).log_softmax()
+
+model = TinyBobNet()
+optim = optim.SGD([model.l1, model.l2], lr=0.001)
+
+# ... complete data loader here
+
+out = model.forward(x)
+loss = out.mul(y).mean()
+optim.zero_grad()
+loss.backward()
+optim.step()
